31ecfead498e4d68ba5012063b85456f81ae4fe4
Santiago Romani <santiago.romani@urv.cat>
2024-08-03 17:57:38 +0200

    prog3: incluye candy1_move.s

---
+++ source/candy1_move.s
@@ -0,0 +1,103 @@
+﻿@;=                                                         	      	=
+@;=== candy1_move: rutinas para contar repeticiones y bajar elementos ===
+@;=                                                          			=
+@;=== Programador tarea 1E: xxx.xxx@estudiants.urv.cat				  ===
+@;=== Programador tarea 1F: yyy.yyy@estudiants.urv.cat				  ===
+@;=                                                         	      	=
+
+
+
+.include "candy1_incl.i"
+
+
+
+@;-- .text. código de las rutinas ---
+.text	
+		.align 2
+		.arm
+
+
+
+@;TAREA 1E;
+@; cuenta_repeticiones(*matriz, f, c, ori): rutina para contar el número de
+@;	repeticiones del elemento situado en la posición (f,c) de la matriz, 
+@;	visitando las siguientes posiciones según indique el parámetro de
+@;	orientación ori.
+@;	Restricciones:
+@;		* solo se tendrán en cuenta los 3 bits de menor peso de los códigos
+@;			almacenados en las posiciones de la matriz, de modo que se ignorarán
+@;			las marcas de gelatina (+8, +16)
+@;		* la primera posición también se tiene en cuenta, de modo que el número
+@;			mínimo de repeticiones será 1, es decir, el propio elemento de la
+@;			posición inicial
+@;	Parámetros:
+@;		R0 = dirección base de la matriz
+@;		R1 = fila f
+@;		R2 = columna c
+@;		R3 = orientación ori (0 -> Este, 1 -> Sur, 2 -> Oeste, 3 -> Norte)
+@;	Resultado:
+@;		R0 = número de repeticiones detectadas (mínimo 1)
+	.global cuenta_repeticiones
+cuenta_repeticiones:
+		push {lr}
+		
+		
+		pop {pc}
+
+
+@;TAREA 1F;
+@; baja_elementos(*matriz): rutina para bajar elementos hacia las posiciones
+@;	vacías, primero en vertical y después en diagonal; cada llamada a la función
+@;	baja múltiples elementos una posición y devuelve cierto (1) si se ha
+@;	realizado algún movimiento, o falso (0) si no se ha movido ningún elemento.
+@;	Restricciones:
+@;		* para las casillas vacías de la primera fila se generarán nuevos
+@;			elementos, invocando la rutina mod_random() (ver fichero
+@;			'candy1_init.s')
+@;	Parámetros:
+@;		R0 = dirección base de la matriz de juego
+@;	Resultado:
+@;		R0 = 1 indica se ha realizado algún movimiento, de modo que pueden
+@;				quedar movimientos pendientes; 0 si no ha movido nada 
+	.global baja_elementos
+baja_elementos:
+		push {lr}
+		
+		
+		pop {pc}
+
+
+
+@;:::RUTINAS DE SOPORTE:::
+
+
+
+@; baja_verticales(mat): rutina para bajar elementos hacia las posiciones vacías
+@;	en vertical; cada llamada a la función baja múltiples elementos una posición
+@;	y devuelve cierto (1) si se ha realizado algún movimiento.
+@;	Parámetros:
+@;		R4 = dirección base de la matriz de juego
+@;	Resultado:
+@;		R0 = 1 indica que se ha realizado algún movimiento; 0 si no ha movido nada  
+baja_verticales:
+		push {lr}
+		
+		
+		pop {pc}
+
+
+@; baja_laterales(mat): rutina para bajar elementos hacia las posiciones vacías
+@;	en diagonal; cada llamada a la función baja múltiples elementos una posición
+@;	y devuelve cierto (1) si se ha realizado algún movimiento.
+@;	Parámetros:
+@;		R4 = dirección base de la matriz de juego
+@;	Resultado:
+@;		R0 = 1 indica que se ha realizado algún movimiento; 0 si no ha movido nada. 
+baja_laterales:
+		push {lr}
+		
+		
+		pop {pc}
+
+
+.end
